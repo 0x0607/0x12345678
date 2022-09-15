6dc384ae2e4d8b2107342a67bf59f49273dc0e3b
Koin.Coding <74230665+0x0607@users.noreply.github.com>
2022-09-15 15:20:32 +0800

    use char version

--- main.char.c
+++ main.char.c
@@ -7,8 +7,8 @@ int main(){
 /********************************************/
 	printf("a=0x%-8x | Address: 0x%x\n",a,&a);
 	while(i<4){
-    b=(b<<8)+*((char*)&a+i);
-  i++;}
+    	    b=(b<<8)+*((char*)&a+i);
+  	i++;}
 	printf("---------------swap-----------------\n");
 	printf("b=0x%-8x | Address: 0x%x\n\n",b,&b);
 	system("pause");
