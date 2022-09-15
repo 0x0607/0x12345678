af515e738d585701b89a48839b22c84b35031ff3
Koin.Coding <74230665+0x0607@users.noreply.github.com>
2022-09-15 15:18:05 +0800

    use char verion

---
+++ main-char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int a=0x12345678;
+	int b;
+	int i=0;
+/********************************************/
+	printf("a=0x%-8x | Address: 0x%x\n",a,&a);
+	while(i<4){
+    b=(b<<8)+*((char*)&a+i);
+  i++;}
+	printf("---------------swap-----------------\n");
+	printf("b=0x%-8x | Address: 0x%x\n\n",b,&b);
+	system("pause");
+	return 0;
+}
